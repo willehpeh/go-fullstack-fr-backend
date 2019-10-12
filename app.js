b5ea03f43ea4b64a403960908396d765fecf2a55
willehpeh <will_alexander@me.com>
2019-10-12 14:46:33 +0200

    Récupération d'un Thing spécifique

--- app.js
+++ app.js
@@ -31,7 +31,13 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
   Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
