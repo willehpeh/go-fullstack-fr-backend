f59fe5cec8f06a3df4e4e83c2fa5b935e9f95c21
willehpeh <will_alexander@me.com>
2019-10-12 17:09:38 +0200

    Suppression d'un Thing

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.put('/api/stuff/:id', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.get('/api/stuff/:id', (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
     .then(thing => res.status(200).json(thing))
